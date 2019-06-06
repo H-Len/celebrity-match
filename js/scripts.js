e0c96c17de35c8133ecece9559f4d557f6182a20
hannah melendy and josh mabry <josh@>
2019-06-06 13:39:22 -0700

    add .hide functionality in JS to results

--- js/scripts.js
+++ js/scripts.js
@@ -13,12 +13,17 @@ $(document).ready(function() {
 
     if (myMatch < 5){
       $(".baggins").show();
+      $(".ariel, .darko, .nomatch").hide();
     } else if (myMatch < 9){
       $(".ariel").show();
+      $(".darko, .baggins, .nomatch").hide();
+
     } else if (myMatch < 13){
       $(".darko").show();
+      $(".ariel, .baggins, .nomatch").hide();
     } else {
       $(".nomatch").show();
+      $(".baggins, .ariel, .darko");
     }
 
    $(".insert").text(name);
